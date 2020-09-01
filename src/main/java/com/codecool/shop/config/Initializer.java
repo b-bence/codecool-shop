67d5beb6e062a5fe402d582e75edee3aa738fcd8
Takács Veronika <roni9764@gmail.com>
2020-09-01 11:09:51 +0200

    Add some Nicolas cage products to store

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -33,12 +33,14 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        Supplier wish = new Supplier("Wish", "Weird stuff");
     }
 
     private void productCategoryInitializer(ProductCategoryDao productCategoryDataStore) {
         //setting new product categories
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory nicolasCage = new ProductCategory("Nicolas Cage", "Actor", "Nicolas Cage. On your pants. On your mug. On your pillow. Everywhere.");
     }
 
     private void productInitializer(ProductDao productDataStore, ProductCategoryDao productCategoryDataStore, SupplierDao supplierDataStore) {
@@ -46,5 +48,7 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", productCategoryDataStore.find("Tablet"), supplierDataStore.find("Amazon")));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", productCategoryDataStore.find("Tablet"), supplierDataStore.find("Lenovo")));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", productCategoryDataStore.find("Tablet"), supplierDataStore.find("Amazon")));
+        productDataStore.add(new Product("Nicolas Cage Mug", 8, "USD", "Nicolas Cage staring at you nicely from your mug. Everyday.", productCategoryDataStore.find("Nicolas Cage"), supplierDataStore.find("Wish")));
+        productDataStore.add(new Product("Nicolas Cage pants", 25, "USD", "Nicolas Cage staring at OTTERS nicely from your pants. Everyday.", productCategoryDataStore.find("Nicolas Cage"), supplierDataStore.find("Wish")));
     }
 }
