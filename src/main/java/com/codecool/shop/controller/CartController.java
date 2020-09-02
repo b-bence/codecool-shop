5d213a520dc1c983f59f4a3af0e017c4891a8610
Takács Veronika <roni9764@gmail.com>
2020-09-02 15:01:51 +0200

    Add new page to review cart items

---
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -0,0 +1,47 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Order;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(urlPatterns = {"/cart"})
+public class CartController extends HttpServlet {
+
+//    private Optional<Order> order = Optional.empty();
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        ProductDao productDataStore = ProductDaoMem.getInstance();
+//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+//        context.setVariable("categories", productCategoryDataStore.getAll());
+//        context.setVariable("category", productCategoryDataStore.find(1));
+//        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+
+        engine.process("product/cart.html", context, resp.getWriter());
+    }
+
+}
+
+
+
