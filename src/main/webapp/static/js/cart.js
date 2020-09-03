57c48104789b9883bf5d0ab4e96a2290d08bb5a6
Takács Veronika <roni9764@gmail.com>
2020-09-03 22:29:32 +0200

    Increase lineitem's quantity in cart

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -1,10 +1,8 @@
-
-let plusQuantityButtons = document.querySelectorAll(".plus-quantity");
-
 init();
 
 function init() {
     addEventToMinusQuantityButton();
+    addEventToPlusQuantityButton();
 }
 
 function addEventToMinusQuantityButton() {
@@ -14,7 +12,6 @@ function addEventToMinusQuantityButton() {
             let productId = minusButton.parentElement.dataset.productId;
             let userId = document.querySelector("table").dataset.userId;
             modifyQuantity(productId, userId, "decrease", (response) => {
-                let productId = response.productId;
                 let quantity = response.quantity;
                 if (quantity == 0) {
                     location.reload();
@@ -22,6 +19,21 @@ function addEventToMinusQuantityButton() {
                     let quantityTd = minusButton.parentElement.parentElement.children[2];
                     quantityTd.innerText = quantity;
                 }
+            })
+        })
+    }
+}
+
+function addEventToPlusQuantityButton() {
+    let plusQuantityButtons = document.querySelectorAll(".plus-quantity");
+    for (let plusButton of plusQuantityButtons) {
+        plusButton.addEventListener("click", () => {
+            let productId = plusButton.parentElement.dataset.productId;
+            let userId = document.querySelector("table").dataset.userId;
+            modifyQuantity(productId, userId, "increase", (response) => {
+                let quantity = response.quantity;
+                let quantityTd = plusButton.parentElement.parentElement.children[2];
+                quantityTd.innerText = quantity;
             })
         })
     }
