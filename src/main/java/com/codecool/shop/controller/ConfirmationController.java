400845bf2daeadcd610372573f88c92b923541a1
Getulus <tamasgaj@gmail.com>
2020-09-03 16:10:56 +0200

    confirmation site done

---
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Order;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(urlPatterns = {"/confirmation"})
+public class ConfirmationController extends HttpServlet {
+
+    private Optional<Order> order = Optional.empty();
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("cost","Overall cost : 5000 $");
+
+
+        engine.process("product/confirmation.html", context, resp.getWriter());
+    }
+
+}
